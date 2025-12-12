92895ffd30e87d6033ece9c561ab13af5b07a992
Azat <aza.dauletbek9@gmail.com>
2025-12-12 11:44:01 +0500

    git commit

---
+++ src/main/java/com/example/gymtraining/GymTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gymtraining/GymTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
